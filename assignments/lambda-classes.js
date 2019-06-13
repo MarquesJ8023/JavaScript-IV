0e2a0e83bf08655db24f2a99474eba657bdb011d
Marques Johnson <marques.johnson1992@gmail.com>
2019-06-13 16:50:08 -0400

    First Commit

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,10 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attrs) {
+        this.attrs = attrs;
+        this.atrs1 = attrs1;
+
+    }
+}
+const newPerson = new Person(args);
+Person {attrs: args, attrs1: args }
