d0f69db3d3a3cfc745b3cd640885cc14695813b6
Marques Johnson <marques.johnson1992@gmail.com>
2019-06-13 18:47:50 -0400

    another push

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -71,7 +71,7 @@ class Person {
     }
     speak(attrs) {
         return `Hello my name is ${this.name}, I am from ${this.location}.`;
-    
+    }
 }
 
 class Instructor extends Person {
@@ -81,11 +81,11 @@ class Instructor extends Person {
         this.favLanguage = attrs.favLanguage;
         this.catchPhrase = attrs.catchPhrase;
     }
-    demo(Subject) {
+    demo(subject) {
         return `Today we are learning about ${subject}`;
     }
     grade(student, subject) {
-        this.student = 
+        return `${student.name} receives a perfect score on ${subject}`;
     }
 }
 
@@ -93,13 +93,34 @@ class Student extends Person {
     constructor(attrs) {
         super(attrs);
         this.background = attrs.background;
-        this.className = attrs.background;
-        this.favSubject = attrs.favSubject[];
+        this.className = attrs.className;
+        this.favSubject = attrs.favSubject;
     }
-    listsSubjects (this.favSubjects[]) {
-        return `This is a list of my favorite subjects `;
+    listsSubjects() {
+        this.favSubjects.forEach(element => console.log(element));
     }
-    PRAssignment(attrs) {
-        return '${this.name'} has submitted a PR for ${subject}`;
+
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge() {
+      return `${this.name} has begun sprint challenge on ${subject}`;
+
     }
 }
+
+class ProjectManager extends Instructor {
+    constructor (attrs) {
+        super(attrs);
+        this.gradClassName = attrs.gradClassName;
+        this.favInstructor = attrs.favInstructor;
+    };
+
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
+
